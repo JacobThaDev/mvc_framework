4be991fe47f7387bb03594c4b9c48a449dee9513
KingFox <rune.evo2012@gmail.com>
2021-02-02 15:01:34 -0600

    added missing config file

---
+++ app/config.php
@@ -0,0 +1,12 @@
+<?php
+define("web_root", "/framework/");
+
+const sql_databases = [
+    'default' => [
+        "driver"   => "mysql",
+        "host"     => "localhost",
+        "database" => "",
+        "username" => "",
+        "password" => "",
+    ]
+];
